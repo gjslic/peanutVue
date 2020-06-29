d3c8569dc877b1440bb430c3f323691da19e9b74
Your Name <you@example.com>
2020-06-29 16:33:43 +0800

    staff

--- peanut/src/router/index.js
+++ peanut/src/router/index.js
@@ -2,20 +2,38 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Goods from '@/components/Goods'
+import StaffManage from '@/components/StaffManage'
+import Jurisdic from '@/components/Jurisdiction'
+import Order from '@/components/Order'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
+    // {
+    //   path: '/',
+    //   name: 'Home',
+    //   component: Home
+    // },
     {
       path: '/goods',
       name: 'Goods',
       component: Goods
+    },
+    {
+      path: '/staffManage',
+      name: 'staffManage',
+      component: StaffManage
+    },
+    {
+      path: '/jurisdic',
+      name: 'jurisdic',
+      component: Jurisdic
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order
     }
   ]
 })
