ef72889936d24db1a9e05b0cd4fefd5cdf72c5e7
ChooseA7770 <474220553@qq.com>
2020-06-30 16:38:56 +0800

    lzy

--- peanut/src/App.vue
+++ peanut/src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view/>
   </div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  }
+  name: 'App'
+
 }
 </script>
 
 <style>
-  body,ul,h1,p{
-    margin: 0;
-    padding: 0;
-  }
-  a{
-    text-decoration: none;
-    color: #333;
-  }
-  li{
-    list-style: none;
-  }
+
+
+
 </style>
---
+++ peanut/src/Homepage.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+      <Menu />
+      <Banner />
+      <Carbrand />
+      <Carrecommend />
+      <Bottom />
+      <!-- <AuctionVehicle /> -->
+</div>
+
+
+</template>
+
+<script>
+import Menu from './components/Menu';
+// import AuctionVehicle from './components/AuctionVehicle';
+import Banner from './components/Banner';
+import Carbrand from './components/Carbrand';
+import Carrecommend from './components/Carrecommend';
+import Bottom from './components/Bottom';
+export default {
+    name:'Homepage',
+    components:{
+        Menu,
+        Banner,
+        // AuctionVehicle,
+        Carbrand,
+        Carrecommend,
+        Bottom
+    }
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
