16e51394dc2608ee5bf8a6f5f6c774cf41411a08
ChooseA7770 <474220553@qq.com>
2020-07-06 14:46:06 +0800

    lzy

--- peanut/src/network/home.js
+++ peanut/src/network/home.js
@@ -3,16 +3,11 @@ import {request} from './request'
 export function getData(url){
 
   return request({
-<<<<<<< HEAD
-    method:'post',
-=======
->>>>>>> master
     url: url
   })
 
 }
 
-<<<<<<< HEAD
 export function sendParam(url ,data){
   return request({
     method:'post',
@@ -21,16 +16,6 @@ export function sendParam(url ,data){
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
 		},
-=======
-export function sendParam(url, data) {
-    return request({
-        method: 'post',
-        url: url,
-        data: data,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
->>>>>>> master
 
   })
 
