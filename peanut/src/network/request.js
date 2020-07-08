d650dd3732a18b54794f013924b7eb15e0b4db63
gjslic <1543448416@qq.com>
2020-07-08 11:03:07 +0800

    gjs

--- peanut/src/network/request.js
+++ peanut/src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config){
   // 1.创建实例
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1/node_yzlll/peanut/th5/public',
+    baseURL: 'http://127.0.0.1/th5/public',
     timeout: 5000 
   })
   // 设置post请求头
